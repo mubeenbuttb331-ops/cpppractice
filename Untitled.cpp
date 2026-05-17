adc43d564d4bc82cb85a43f6d4ec800e1ad2bb35
mubeenbuttb331-ops <mubeenbuttb331@gmail.com>
2026-05-17 13:13:43 +0500

    Add files via upload

---
+++ Untitled.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	cout<<((7>2)&&(1<2))<<endl;
+//	cout<<((2>8)||(5<1))<<endl;
+//	cout<<!((1>4));
+    int x=10,y=10;
+    cout<<(x>y)<<endl;
+    cout<<(x>=y)<<endl;
+    return 0;
+}
---
+++ harry.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int glo=8;
+void sum(){
+	int a;
+	cout<<glo;
+}
+
+int main(){ 
+int glo=9;
+glo=65;
+//int a=13, b=14;
+//float pi=3.144, e=2.71828;
+//char m='A',n='b';
+//bool c=true;
+
+//cout<<"\nhey!there i am mubeen\n"<<"the vaue of sum is "<<a+b<<"\nthr sum of pi and e is"<<pi+e<<"\n"<<"the grade of m is "<<m<<"\nthe grade of n is "<<n;    
+
+sum();
+cout<<glo<<endl;
+return 0;
+
+}
