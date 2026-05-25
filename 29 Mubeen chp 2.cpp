eef1bb4837521f0e0c078f506961d1b140b2c379
mubeenbuttb331-ops <mubeenbuttb331@gmail.com>
2026-05-25 18:19:44 +0500

    Add files via upload

---
+++ 29 Mubeen chp 2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+//	cout<<"hello girls!\n" ;  //this is a simple function which break the line.
+//	cout<<"hello girls"<<endl;  //his also break the line.
+	//this give us the exapmle of simple function and how we break the line simply.
+	
+	//ARITHMETIC OPERATION
+	//ADITION:
+	cout<<10+9<<endl;//ALSO RUN WITHOUT USING ENDL
+	//MULTIPLICATION:
+	cout<<10*8<<endl;
+	//DIVISION:
+	cout<<50/5<<endl;
+	//MODULUD
+	cout<<43%5<<endl;
+	//BY ASINGING VARAIBLE: varaibel are use to store data in a program.
+	int a=16,b=4;
+	cout<<a+b<<endl<<a*b<<endl<<a%b<<endl;
+	//we also done by this in 1 line and alsp in different steps
+	cout<<a+b<<endl;
+	cout<<a*b<<endl;
+	cout<<a%b<<endl;
+	cout<<a-b<<endl;
+	//is return 0 place at this position so next programmm annt be print.
+	//TESTING VARAIBLE:
+//	string name="Mubeen";
+//	int age=19;
+//	float height=5.1;
+//	float weight=43.8;
+//	char grade='A';
+//	cout<<name<<endl<<age<<endl<<height<<endl<<weight<<endl<<grade<<endl;//this is the one way.
+	//we also get the same output byy this.
+//	cout<<name<<endl;
+//	cout<<age<<endl;
+//	cout<<height<<endl;
+//	cout<<weight<<endl;
+//	cout<<grade<<endl;
+	// end of the program.
+	//return 0 showa that the function ended successfully.
+	return 0;//by using return0 the execuation of programm stop immediately. 
+}
